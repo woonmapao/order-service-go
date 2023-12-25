393b92da37edca178eacd5638d83b6396d5a13b9
Patarapong Limvipaveeanan <pat_opor@hotmail.com>
2023-12-25 12:37:08 +0700

    Paste Initializer boiler plate

--- initializer/loadEnvVariables.go
+++ initializer/loadEnvVariables.go
@@ -1,1 +1,14 @@
 package initializer
+
+import (
+	"log"
+
+	"github.com/joho/godotenv"
+)
+
+func LoadEnvVariables() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env filea")
+	}
+}
