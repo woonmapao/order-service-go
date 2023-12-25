13b0224713023e4f183e355db840b5dba48a771a
Patarapong Limvipaveeanan <pat_opor@hotmail.com>
2023-12-25 15:14:46 +0700

    Setup a server and database

--- main.go
+++ main.go
@@ -1,12 +1,26 @@
 package main
 
 import (
+	"os"
+
 	"github.com/gin-gonic/gin"
+	"github.com/woonmapao/order-service-go/initializer"
 )
+
+func init() {
+	initializer.LoadEnvVariables()
+	initializer.DBInitializer()
+}
 
 func main() {
 
 	r := gin.Default()
-	r.Run()
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	r.Run(":" + port)
 
 }
